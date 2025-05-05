489a1d1e688d4549efca556029290720d4711431
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-05-05 16:51:31 +0000

    cvbn

---
+++ draw_mark.py
@@ -0,0 +1,53 @@
+import pygame
+
+
+    
+def draw_x(mouse_x,mouse_y,screen):
+    if mouse_x < 300 and mouse_y < 300:
+        pygame.draw.line(screen, (0, 0, 0), (50, 50), (255,255), 30)
+        pygame.draw.line(screen, (0, 0, 0), (255, 50), (50,255), 30)
+    elif 300 < mouse_x < 600 and mouse_y < 300:
+        pygame.draw.line(screen, (0, 0, 0), (350, 50), (555,255), 30)
+        pygame.draw.line(screen, (0, 0, 0), (555, 50), (350,255), 30)
+    elif 600 < mouse_x and mouse_y < 300:
+        pygame.draw.line(screen, (0, 0, 0), (650, 50), (855,255), 30)
+        pygame.draw.line(screen, (0, 0, 0), (855, 50), (650,255), 30)
+    elif mouse_x < 300 and 300 < mouse_y < 600:
+        pygame.draw.line(screen, (0, 0, 0), (50, 350), (255,555), 30)
+        pygame.draw.line(screen, (0, 0, 0), (255, 350), (50,555), 30)
+    elif 300 < mouse_x < 600 and 300 < mouse_y < 600:
+        pygame.draw.line(screen, (0, 0, 0), (350, 350), (555,555), 30)
+        pygame.draw.line(screen, (0, 0, 0), (555, 350), (350,555), 30)
+    elif 600 < mouse_x and 300 < mouse_y < 600:
+        pygame.draw.line(screen, (0, 0, 0), (650, 350), (855,555), 30)
+        pygame.draw.line(screen, (0, 0, 0), (855, 350), (650,555), 30)
+    elif mouse_x < 300 and 600 < mouse_y:
+        pygame.draw.line(screen, (0, 0, 0), (50, 650), (255,855), 30)
+        pygame.draw.line(screen, (0, 0, 0), (255, 650), (50,855), 30)
+    elif 300 < mouse_x < 600 and 600 < mouse_y:
+        pygame.draw.line(screen, (0, 0, 0), (350, 650), (555,855), 30)
+        pygame.draw.line(screen, (0, 0, 0), (555, 650), (350,855), 30)
+    elif 600 < mouse_x and 600 < mouse_y:
+        pygame.draw.line(screen, (0, 0, 0), (650, 650), (855,855), 30)
+        pygame.draw.line(screen, (0, 0, 0), (855, 650), (650,855), 30)
+   
+
+def draw_o(mouse_x,mouse_y,screen):
+    if mouse_x < 300 and mouse_y < 300:
+        pygame.draw.circle(screen, (0, 0, 0), (150, 150), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif 300 < mouse_x < 600 and mouse_y < 300:
+        pygame.draw.circle(screen, (0, 0, 0), (450, 150), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif 600 < mouse_x and mouse_y < 300:
+        pygame.draw.circle(screen, (0, 0, 0), (750, 150), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif mouse_x < 300 and 300 < mouse_y < 600:
+        pygame.draw.circle(screen, (0, 0, 0), (150, 450), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif 300 < mouse_x < 600 and 300 < mouse_y < 600:
+        pygame.draw.circle(screen, (0, 0, 0), (450, 450), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif 600 < mouse_x and 300 < mouse_y < 600:
+       pygame.draw.circle(screen, (0, 0, 0), (750, 450), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif mouse_x < 300 and 600 < mouse_y:
+        pygame.draw.circle(screen, (0, 0, 0), (150, 750), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif 300 < mouse_x < 600 and 600 < mouse_y:
+       pygame.draw.circle(screen, (0, 0, 0), (450, 750), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
+    elif 600 < mouse_x and 600 < mouse_y:
+        pygame.draw.circle(screen, (0, 0, 0), (750, 750), 120, width=20, draw_top_right=1, draw_top_left=1, draw_bottom_left=1, draw_bottom_right=1)
---
+++ get_mouse.py
@@ -0,0 +1,18 @@
+import pygame
+
+def get_mouse_pos():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        mouse_x, mouse_y = event.pos
+        if mouse_x == 300 or mouse_x == 600 or mouse_x == 900 or mouse_y == 300 or mouse_y == 600 or mouse_y == 900:
+            print("Click in a square not on the edges to play")
+            mouse_x, mouse_y = get_mouse_pos()	
+        else:
+            mouse_x, mouse_y = get_mouse_pos()
+            return mouse_x, mouse_y
+    
+    pygame.display.flip()

ecb529702264ce362507d79fddc592fb5c3020bb
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-05-06 13:43:56 -0600

    xcdvfbgnh

--- get_mouse.py
+++ get_mouse.py
@@ -1,19 +1,11 @@
 import pygame
 import time
 from draw_mark import *
+from turns import *
 
 def get_mouse_pos(screen):
     running = True
-    turn = False
     turns = 0
-    
-
-    def is_even(turns):
-        if (turns%2) == 0:
-            turn = True
-        else:
-            turn = False
-        return turn
     
     while running:
         turn = is_even(turns)
@@ -23,9 +15,18 @@ def get_mouse_pos(screen):
                 running = False
         
         if event.type == pygame.MOUSEBUTTONDOWN:
-            time.sleep(400)
-            if event.type == pygame.MOUSEBUTTONUP:
                 print('Mouse button pressed!')
+                mouse_x, mouse_y = event.pos
+                if mouse_x == 300 or mouse_x == 600 or mouse_x == 900 or mouse_y == 300 or mouse_y == 600 or mouse_y == 900:
+                    print("Click in a square not on the edges to play")
+                    mouse_x, mouse_y = get_mouse_pos()
+                else:
+                    if turn == "Even":
+                        draw_x(mouse_x, mouse_y, screen)
+                    elif turn == "Odd":
+                        draw_o(mouse_x, mouse_y, screen)
+                    turns+=1
+                time.sleep(0.5)
 
         #if event.type == pygame.MOUSEBUTTONDOWN:
             #if event.type == pygame.MOUSEBUTTONUP:
---
+++ tic_tac_toe.py
@@ -0,0 +1,19 @@
+import random
+
+def set_board():
+    slot1 = "1"
+    slot2 = "2"
+    slot3 = "3"
+    slot4 = "4"
+    slot5 = "5"
+    slot6 = "6"
+    slot7 = "7"
+    slot8 = "8"
+    slot9 = "9"
+
+    choices = [slot1,slot2,slot3,slot4,slot5,slot6,slot7,slot8,slot9]
+
+    return choices
+
+def game():
+    pass
